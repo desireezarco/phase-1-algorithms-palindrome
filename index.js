4fc8fdb6b9c928d3b63c0e005d1f561ae8182c8e
Desiree Zarco <desireeannzarco7@gmail.com>
2023-06-04 21:31:46 -0700

    complete lab

--- index.js
+++ index.js
@@ -1,14 +1,45 @@
-function isPalindrome(word) {
-  // Write your algorithm here
+const myArray = [2,3,4,5,6]
+console.log(myArray[myArray.length-1])
+
+function isPalindromeFirst(word){
+
+  // 0(n)
+  // 0(n) + 0(n) + 0(n) + 1
+  //0(3n+1)
+  //0(n)
+
+  if (word.split("").reverse().join("") === word) return true
+  return false
 }
 
-/* 
-  Add your pseudocode here
-*/
+function isPalindrome(word) {
+  for(i=0; i<word.length/2; i++){
+    const j = word.length-1-i
+    const startingChar = word[i]
+    const endingChar = word[j]
+
+    if (startingChar !== endingChar) return false
+    console.log(startingChar)
+    console.log(endingChar)
+    return true
+    }
+  }
+
+let response = isPalindromeFirst("racecar")
+console.log(`The word ${response?"is":"is not"} a palindrome`)
+
 
 /*
-  Add written explanation of your solution here
+Function takes a string
+Inspect each character
+Split the string into an array of characters
+Return array of characters that have been reversed--
+iterate through the characters--
+Verify the strings match or not
+Returns true or false
 */
+
+
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
@@ -23,3 +54,5 @@ if (require.main === module) {
 }
 
 module.exports = isPalindrome;
+
+
